bf1e9733173adfddf8c76d6abcfaae968b58327c
Jake Hawken <jacobdhawken@gmail.com>
2018-04-08 16:33:11 -0700

    Updated to Swift 4 and removed some configuration files from git tracking.

--- JakeFake.swift
+++ JakeFake.swift
@@ -4,7 +4,8 @@
 
 import Foundation
 
-protocol JakeFakeFunction: Hashable, Equatable {
+
+protocol JakeFakeFunction: Hashable {
 }
 
 protocol JakeFake {
@@ -19,23 +20,21 @@ protocol JakeFake {
 }
 
 class JakeFaker<Function: JakeFakeFunction> {
-
-    typealias Execution = (()->Any?)
-
-    private struct Stub {
+    
+    private struct Stub<T> {
         let method: Function
-        let execution: Execution
+        let execution: (()->T?)?
     }
-
+    
     func reset() {
         methodCalls.removeAll()
         methodStubs.removeAll()
     }
-
+    
     //MARK: Spying
-
+    
     private var methodCalls = [Int:[Function]]()
-
+    
     internal func recordCall(_ method: Function) {
         if methodCalls[method.hashValue] != nil {
             methodCalls[method.hashValue]?.append(method)
@@ -55,25 +54,26 @@ class JakeFaker<Function: JakeFakeFunction> {
         }
         return calls.filter({ $0 == method}).count
     }
-
+    
     //MARK: Stubbing
-
-    private var methodStubs = [Int:[Stub]]()
-    func stub(_ method: Function, andDo block:Execution?) {
-        let execution: Execution = block ?? { _ in return Void() }
-        let stub = Stub(method: method, execution: execution)
+    
+    private var methodStubs = [Int:[Stub<Any>]]()
+    
+    func stub<T>(_ method: Function, andDo block:(()->T?)?) {
+        let stub: Stub<T> = Stub(method: method, execution: block)
         if methodStubs[method.hashValue] != nil {
-            methodStubs[method.hashValue]?.append(stub)
+            methodStubs[method.hashValue]?.append(stub as! Stub<Any>)
         } else {
-            methodStubs[method.hashValue] = [stub]
+            methodStubs[method.hashValue] = [stub as! Stub<Any>]
         }
     }
+    
     func stubbedValue(_ functionName:String = #function, method: Function) -> Any? {
         guard let stubs = methodStubs[method.hashValue],
             let stub = stubs.filter({ $0.method == method}).last else {
-            fatalError("Method \(functionName) not stubbed.")
+                fatalError("Method \(functionName) not stubbed.")
         }
-        return stub.execution()
+        return stub.execution?()
     }
 }
 
@@ -95,13 +95,9 @@ extension JakeFake {
         faker.stub(method, andDo: block)
     }
     func stubbedValue<T>(method: Function, asType: T.Type) -> T? {
-        if let value = faker.stubbedValue(method: method) as? T {
-            return value
+        guard let value = faker.stubbedValue(method: method) as? T else {
+            fatalError("\(method) stubbed with the wrong type")
         }
-        fatalError("\(method) stubbed with the wrong type")
-    }
-    func recordAndStub<T>(method: Function, asType: T.Type) -> T? {
-        recordCall(method)
-        return stubbedValue(method: method, asType: asType)
+        return value
     }
 }
